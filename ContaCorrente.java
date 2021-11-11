5afdeb52ba12ea7dbe538903945948dfefd7b452
Samuel-prog182 <samuellb.o.w.182@hotmail.com>
2021-11-11 19:31:06 +0000

    Limite da Conta

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,9 +1,24 @@
 public class ContaCorrente{
-    //3 atributos -> numero,saldo,status
+   //3 atributos -> numero,saldo,status
     private int numero;
     private double saldo;
     private boolean status;
+   //status true estou no cheque especial
+   //status false estou com a conta no azul
     private double limite;
+    
+
+    public void saca(double valor){
+        if(valor > saldo && limite < 2000){
+            status=true;
+            limite+=saldo-valor;
+        }
+        else{
+            status=false;
+        }
+        saldo=saldo-valor;
+        
+    }
 
     public void deposita(double valor){
         saldo+=valor;
--- Teste.java
+++ Teste.java
@@ -1,11 +1,21 @@
 public class Teste {
+    private static final int _2000 = 2000;
+
     public static void main(String[] args) {
 
         ContaCorrente conta=new ContaCorrente();
-        conta.deposita(1100);
+
         conta.setSaldo(100);
+        conta.deposita(1100);
+        conta.saca(1500);
+        
 
         System.out.println("Saldo: "+conta.getSaldo());
+        System.out.println("Limite utilizado : "+conta.getLimite());
+
+    
+
+
 
         
     }
